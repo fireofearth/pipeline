849d7b7c84cbc412c5e99ab30c682c6da98fb468
Colin Chen <colinc@fastmail.com>
2020-04-02 10:33:33 -0700

    proprocess+train TCGA EC 50

---
+++ tcga_ec_50_preprocess.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name tcga_ec_50_preprocess
+#SBATCH --cpus-per-task 1
+#SBATCH --output /home/cochen/cchen/ml/slurm/%j.out
+#SBATCH --error /home/cochen/cchen/ml/slurm/%j.err
+#SBATCH -w dlhost02
+#SBATCH -p dgxV100
+#SBATCH --gres=gpu:1
+#SBATCH --time=4-90:00:00
+#SBATCH --chdir /home/cochen/cchen/ml/pipeline
+
+source /home/cochen/cchen/py2
+
+kronos run \
+	-c $PWD/../ \
+	-y tcga_ec_50_preprocess.yaml \
+	--no_prefix 
---
+++ tcga_ec_50_tn_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name local_ec_100_tn_train
+#SBATCH --cpus-per-task 1
+#SBATCH --output /home/cochen/cchen/ml/slurm/%j.out
+#SBATCH --error /home/cochen/cchen/ml/slurm/%j.err
+#SBATCH -w dlhost02
+#SBATCH -p dgxV100
+#SBATCH --gres=gpu:1
+#SBATCH --time=4-90:00:00
+#SBATCH --chdir /projects/ovcare/classification/cchen/ml/pipeline
+
+source /projects/ovcare/classification/cchen/py2
+
+kronos run \
+	-c $PWD/../ \
+	-s $PWD/setup.txt \
+	-y local_ec_100_tn_train.yaml \
+	--no_prefix 
