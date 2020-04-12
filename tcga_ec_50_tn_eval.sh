deabb26bc968ef5e0ced64aaa72452611a937c17
Colin Chen <colinc@fastmail.com>
2020-04-12 11:47:50 -0700

    TCGA EC 50 Eval

---
+++ tcga_ec_50_tn_eval.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name tcga_ec_50_tn_eval
+#SBATCH --cpus-per-task 1
+#SBATCH --output /home/cochen/cchen/ml/slurm/%j.out
+#SBATCH --error /home/cochen/cchen/ml/slurm/%j.err
+#SBATCH -w dlhost02
+#SBATCH -p dgxV100
+#SBATCH --gres=gpu:1
+#SBATCH --time=4-90:00:00
+#SBATCH --chdir /projects/ovcare/classification/cchen/ml/pipeline
+
+source /projects/ovcare/classification/cchen/py2
+
+kronos run \
+	-c $PWD/../ \
+	-s $PWD/setup.txt \
+	-y tcga_ec_50_tn_eval.yaml \
+	--no_prefix 
